a8ca7af043b87edee8b23f44176c4f662e51359b
Gerald Mpyana <gomolemo.mpyana@gmail.com>
2021-12-15 22:12:06 +0200

    ADD: sections for tesla

---
+++ src/components/Home.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtntext="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtntext="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtntext="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtntext="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+
+      <Section
+        title="Solar Panels"
+        description="Lowest Cost Solar Panels in America"
+        backgroundImg="solar-panel.jpg"
+        leftBtntext="Order now"
+        rightBtnText="Learn more"
+      />
+
+      <Section
+        title="Solar Roof"
+        description="
+        Produce Clean Energy From Your Roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtntext="Order now"
+        rightBtnText="Learn more"
+      />
+
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtntext="Shop now"
+       
+      />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
